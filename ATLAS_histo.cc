265acf2d9bf3d2b9566c990b6c4b41023cdab0f2
Ludovic Scyboz <scyboz@mpp.mpg.de>
2016-06-22 10:21:21 +0200

    Moved deltaRb histo outside of the (elecFS.size >= ...) block

--- ATLAS_histo.cc
+++ ATLAS_histo.cc
@@ -209,10 +209,10 @@ namespace Rivet {
 
 	_histPt1->fill(b_jets[0]->pT(), weight);
 	_histEta1->fill(b_jets[0]->eta(), weight);
+	_histdeltaRb->fill(deltaR(b_jets[0]->momentum(), b_jets[1]->momentum()), weight);
 	if(elecFS.size() >= 1 && muonFS.size() >= 1) {
 		//_histPhiemu->fill(deltaPhi(elecFS[0], muonFS[0]), weight);
 		_histPhiemu->fill(mapAngle0To2Pi(elecFS[0].momentum().phi() - muonFS[0].momentum().phi()), weight);
-		_histdeltaRb->fill(deltaR(b_jets[0]->momentum(), b_jets[1]->momentum()), weight);
 		_histdeltaRl->fill(deltaR(elecFS[0], muonFS[0]), weight);
 	}
 	_histPtMiss->fill(MET, weight);
